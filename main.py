230ca2707a6f728be53ced1cc27ffcb1f647854a
crown <crown970523@naver.com>
2023-07-20 14:30:12 +0900

    첫번째 커밋 더하기 함수 완성

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 20 14:20:01 2023
+
+@author: crown
+"""
+## 함수 선언부
+def add_fuc(n1,n2):
+    return n1 + n2
+
+## 전역 변수부
+num1, num2, res = 100,200,0
+
+## 메인 코드부
+res = add_fuc(num1,num2)
+print(num1,"+",num2,'=',res)
