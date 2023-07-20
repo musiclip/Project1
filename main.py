57f3927af6fc7f76c021b09050e16b311c51f413
crown <crown970523@naver.com>
2023-07-20 14:32:50 +0900

    두번째 커밋 빼기 함수 추가

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ Created on Thu Jul 20 14:20:01 2023
 def add_fuc(n1,n2):
     return n1 + n2
 
+def sub_fuc(n1,n2):
+    return n1 - n2
+
 ## 전역 변수부
 num1, num2, res = 100,200,0
 
 ## 메인 코드부
 res = add_fuc(num1,num2)
 print(num1,"+",num2,'=',res)
+
+res = sub_fuc(num1,num2)
+print(num1,"-",num2,'=',res)
